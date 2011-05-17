9cca5295866ed1f7aab7c08f5bbee209dabd5531
Derek Stobbe <djstobbe@gmail.com>
2011-05-17 10:19:30 -0600

    changing method signature on interface, as well

--- Acl/AclManagerInterface.php
+++ Acl/AclManagerInterface.php
@@ -10,7 +10,7 @@ interface AclManagerInterface {
     
     public function createSecurityIdentity($identity);
     
-    public function createPermissionContext(SecurityIdentityInterface $securityIdentity, array $args);
+    public function createPermissionContext($type, SecurityIdentityInterface $securityIdentity, $mask, $granting = true);
     public function addPermissionContext(PermissionContextInterface $permissionContext, $key = null);
     public function processPermissions($reset = false);
     public function applyPermission(PermissionContextInterface $permissionContext);
