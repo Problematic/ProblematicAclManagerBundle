55a457d18b8e9a6c22bb1bb9fccbe2d2667a1ef4
Derek Stobbe <djstobbe@gmail.com>
2011-06-23 13:37:15 -0600

    create DI extension to load services automatically

---
+++ DependencyInjection/ProblematicAclManagerExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Problematic\AclManagerBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension,
+    Symfony\Component\DependencyInjection\ContainerBuilder,
+    Symfony\Component\DependencyInjection\Loader\YamlFileLoader,
+    Symfony\Component\Config\FileLocator;
+
+class ProblematicAclManagerExtension extends Extension
+{
+
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+
+}
