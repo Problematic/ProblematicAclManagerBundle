1405e7d0bfd290612436b2370ee985a3e28d1fae
Derek Stobbe <djstobbe@gmail.com>
2011-07-20 11:22:50 -0600

    updated PermissionContext to CS

--- Acl/PermissionContext.php
+++ Acl/PermissionContext.php
@@ -3,44 +3,61 @@
 namespace Problematic\AclManagerBundle\Acl;
 
 use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
-
 use Problematic\AclManagerBundle\Model\PermissionContextInterface;
 
-class PermissionContext implements PermissionContextInterface {
+class PermissionContext implements PermissionContextInterface
+{
+
     protected $permissionMask;
     protected $securityIdentity;
     protected $permissionType;
     protected $granting;
-    
-    public function __construct() {}
-    
-    public function setMask($mask) {
+
+    public function __construct()
+    {
+        
+    }
+
+    public function setMask($mask)
+    {
         $this->permissionMask = $mask;
     }
-    public function getMask() {
+
+    public function getMask()
+    {
         return $this->permissionMask;
     }
-    
-    public function setSecurityIdentity(SecurityIdentityInterface $securityIdentity) {
+
+    public function setSecurityIdentity(SecurityIdentityInterface $securityIdentity)
+    {
         $this->securityIdentity = $securityIdentity;
     }
-    public function getSecurityIdentity() {
+
+    public function getSecurityIdentity()
+    {
         return $this->securityIdentity;
     }
-    
-    public function setPermissionType($type) {
+
+    public function setPermissionType($type)
+    {
         $this->permissionType = $type;
     }
-    public function getPermissionType() {
+
+    public function getPermissionType()
+    {
         return $this->permissionType;
     }
-    
-    public function setGranting($granting) {
+
+    public function setGranting($granting)
+    {
         $this->granting = $granting;
     }
-    public function isGranting() {
+
+    public function isGranting()
+    {
         return $this->granting;
     }
+
 }
 
 ?>
--- Model/PermissionContextInterface.php
+++ Model/PermissionContextInterface.php
@@ -4,17 +4,23 @@ namespace Problematic\AclManagerBundle\Model;
 
 use Symfony\Component\Security\Acl\Model\SecurityIdentityInterface;
 
-interface PermissionContextInterface {
+interface PermissionContextInterface
+{
+
     public function setMask($mask);
+
     public function getMask();
-    
+
     public function setSecurityIdentity(SecurityIdentityInterface $securityIdentity);
+
     public function getSecurityIdentity();
-    
+
     public function setPermissionType($type);
+
     public function getPermissionType();
-    
+
     public function setGranting($granting);
+
     public function isGranting();
 }
 
